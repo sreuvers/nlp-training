93ea32f5ffec5c999191d08f6484efebbb9d1a3e
sreuvers <h519h5qa>
2022-04-06 10:40:22 +0200

    fix multiprocessing thingy

--- run_config_advanced.py
+++ run_config_advanced.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     print("INITIALIZE DATA: ")
 
     mode = sys.argv[1]
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         test = sys.argv[2]
     else:
         test = 'false'
