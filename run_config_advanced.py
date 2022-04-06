ca6da8bc99a624de0b52acf7dcca9723c5cee65a
sreuvers <h519h5qa>
2022-04-06 09:42:21 +0200

    fix multiprocessing thingy

--- run_config_advanced.py
+++ run_config_advanced.py
@@ -7,19 +7,22 @@ import sys
 import subprocess
 import shlex
 import time
+
+
 def ensure_dir(file_path):
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def initialize_grid_search():
-    param_grid = {"weights_1": [1,1.5,2,4,6],
+    param_grid = {"weights_1": [1, 1.5, 2, 4, 6],
                   "weights_2": [1],
                   "learning_rate": [3e-5],
                   "num_epochs": [3]}
 
     configs = list(ParameterGrid(param_grid))
-    log = {'current_config' : 0}
+    log = {'current_config': 0}
     ensure_dir("/home/bijlesjvl/settings/")
 
     with open('/home/bijlesjvl/settings/configs.json', 'w') as outfile:
@@ -28,6 +31,7 @@ def initialize_grid_search():
     with open('/home/bijlesjvl/settings/log.json', 'w') as outfile:
         json.dump(log, outfile)
     return configs, log
+
 
 def get_search():
     settings_folder = '/home/bijlesjvl/settings/'
@@ -43,10 +47,11 @@ def get_search():
         print("CREATE NEW GRID SEARCH...")
         [configs, log] = initialize_grid_search()
 
-    return configs,log
+    return configs, log
+
 
 def initialize_data(test):
-    if test.lower() == "true" and not os.path.exists('/home/bijlesjvl/data/finetuning/StockTwits_test/') :
+    if test.lower() == "true" and not os.path.exists('/home/bijlesjvl/data/finetuning/StockTwits_test/'):
         print("GET TEST DATA")
         ensure_dir("/home/bijlesjvl/data/finetuning/")
         os.system("gsutil -m cp -r gs://thesis-tpu/data/StockTwits_test /home/bijlesjvl/data/finetuning/")
@@ -55,6 +60,7 @@ def initialize_data(test):
     else:
         ensure_dir("/home/bijlesjvl/data/finetuning/")
         os.system("gsutil -m cp -r gs://thesis-tpu/data/StockTwits /home/bijlesjvl/data/finetuning/")
+
 
 def initialize_model(mode):
     if os.path.exists(f"/home/bijlesjvl/model/CryptoBERT_{mode}/pretrained"):
@@ -70,42 +76,29 @@ def initialize_model(mode):
                         gs://thesis-tpu/model/CryptoBERT_TW_pretrained_2/CryptoBERT_TW/* \
                         /home/bijlesjvl/model/CryptoBERT_TW/pretrained/")
 
+
 def initialize_scripts():
     ensure_dir("/home/bijlesjvl/scripts/")
     if os.path.exists("/home/bijlesjvl/scripts/cryptobert_tw_gcp_weighting_search.py"):
         os.system("rm /home/bijlesjvl/scripts/cryptobert_tw_gcp_weighting_search.py")
-    os.system("wget https://raw.githubusercontent.com/sreuvers/nlp-training/main/cryptobert_tw_gcp_weighting_search.py -P /home/bijlesjvl/scripts/ -q")
+    os.system(
+        "wget https://raw.githubusercontent.com/sreuvers/nlp-training/main/cryptobert_tw_gcp_weighting_search.py -P /home/bijlesjvl/scripts/ -q")
 
-def wait_timeout(proc, seconds):
-    """Wait for a process to finish, or raise exception after timeout"""
-    start = time.time()
-    end = start + seconds
-    interval = min(seconds / 1000.0, .25)
-
-    while True:
-        result = proc.poll()
-        if result is not None:
-            return result
-        if time.time() >= end:
-            return 0
-        time.sleep(interval)
 
 def run_command(command):
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-    result = wait_timeout(process, 60)
-    # while True:
-    #     output = process.stdout.readline()
-    #     if output == '' and process.poll() is not None:
-    #         break
-    #     if output:
-    #         print(output.strip())
-    #         if "TOKENIZED DATASET!" in output.strip():
-    #             break
-    # rc = process.poll()
-    return result
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+    rc = process.poll()
+    return rc
+
 
 if __name__ == "__main__":
-    [configs,log] = get_search()
+    [configs, log] = get_search()
 
     config = configs[int(log['current_config'])]
     print("CURRENT CONFIG: ")
@@ -157,50 +150,38 @@ if __name__ == "__main__":
     print("INITIALIZE SCRIPTS: ")
     initialize_scripts()
 
-    for i in range(log['current_config'],len(configs)):
-        config = configs[i]
-        RUN_NAME = ""
-        for key, value in config.items():
-            if key == list(config.keys())[-1]:
-                RUN_NAME = RUN_NAME + f"{key}_{value}"
-            else:
-                RUN_NAME = RUN_NAME + f"{key}_{value}_"
-        print("START NEW RUN")
-        print(f"RUN NAME: {RUN_NAME}")
+    RUN_NAME = ""
+    for key, value in config.items():
+        if key == list(config.keys())[-1]:
+            RUN_NAME = RUN_NAME + f"{key}_{value}"
+        else:
+            RUN_NAME = RUN_NAME + f"{key}_{value}_"
+    print("START NEW RUN")
+    print(f"RUN NAME: {RUN_NAME}")
 
-        os.environ["RUN_NAME"] = RUN_NAME
-        os.environ["PATH_OUTPUT"] = PATH_OUTPUT + RUN_NAME
+    os.environ["RUN_NAME"] = RUN_NAME
+    os.environ["PATH_OUTPUT"] = PATH_OUTPUT + RUN_NAME
 
-        ensure_dir(PATH_OUTPUT)
-        ensure_dir(PATH_OUTPUT + RUN_NAME + "/")
+    ensure_dir(PATH_OUTPUT)
+    ensure_dir(PATH_OUTPUT + RUN_NAME + "/")
 
-        command = f"python3 scripts/cryptobert_tw_gcp_weighting_search.py \
+    command = f"python3 scripts/cryptobert_tw_gcp_weighting_search.py \
                 --model_name={os.environ['MODEL_NAME']} \
                 --path_output={os.environ['PATH_OUTPUT']} \
                 --path_data={os.environ['PATH_DATA']} \
                 --path_model={os.environ['PATH_MODEL']} \
                 --weights_1=%s \
                 --weights_2=%s \
-                --epochs='3' \
+                --epochs='1' \
                 --train_batch_size='128' \
                 --eval_batch_size='128' \
                 --learning_rate='5e-5' \
                 --warmup_steps='100'" % (config['weights_1'], config['weights_2'])
-        print(f"command is: {command}")
+    print(f"command is: {command}")
 
-        result = run_command(command)
-        print(f"result command is: {result}")
+    result = run_command(command)
+    print(f"result command is: {result}")
+    log['current_config'] = log['current_config'] + 1
 
-        if 0 == result:
-            print("FINISHED RUN SUCCESFULLY")
-        else:
-            print("RUN DIDN'T EXECUTE SUCCESFULLY")
-            raise SystemExit()
-
-        log['current_config'] = log['current_config'] + 1
-
-        with open('/home/bijlesjvl/settings/log.json', 'w') as outfile:
-            json.dump(log, outfile)
-
-    print("FINISHED SEARCH")
-
+    with open('/home/bijlesjvl/settings/log.json', 'w') as outfile:
+        json.dump(log, outfile)
